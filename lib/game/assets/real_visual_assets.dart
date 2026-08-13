fcf2e7e50ee61902fea938e6f5c78853d8f5dbb7
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-13 09:27:16 +0300

    feat: define first real visual asset manifest

---
+++ lib/game/assets/real_visual_assets.dart
@@ -0,0 +1,8 @@
+abstract final class RealVisualAssets {
+  static const String appIconPreview =
+      'docs/assets/05_ui_hud/app_icons/exports_candidate/app_icon_candidate_256.jpg';
+
+  static const List<String> firstVisualManifest = <String>[
+    appIconPreview,
+  ];
+}
