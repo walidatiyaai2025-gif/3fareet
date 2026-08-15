b6d7ab1bc5ac6a17af67358289d24a622334fdb7
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-15 12:48:31 +0300

    UART-003 test production Hero quality floors

--- unity_game/Assets/Afareet/Tests/EditMode/HeroCarLodPolicyTests.cs
+++ unity_game/Assets/Afareet/Tests/EditMode/HeroCarLodPolicyTests.cs
@@ -19,25 +19,37 @@ namespace Afareet.Tests
             Assert.That(HeroCarLodPolicy.TransitionFor(2), Is.EqualTo(.01f).Within(.0001f));
         }
 
-        [TestCase(0, 274, 476)]
-        [TestCase(1, 194, 332)]
-        [TestCase(2, 104, 180)]
-        public void AuthoredMeshesMatchExactBudgetContract(int lod, int vertices, int triangles)
+        [TestCase(0, 2200, 5000)]
+        [TestCase(1, 1100, 2400)]
+        [TestCase(2, 650, 1400)]
+        public void ProductionTargetsSitInsideMobileBudget(int lod, int vertices, int triangles)
         {
             Assert.That(HeroCarLodPolicy.IsWithinBudget(lod, vertices, triangles), Is.True);
         }
 
         [Test]
-        public void WrongOrOverBudgetGeometryIsRejected()
+        public void RejectedBlockoutHeroCanNeverSatisfyProductionContract()
         {
-            Assert.That(HeroCarLodPolicy.IsWithinBudget(0, 274, 601), Is.False);
-            Assert.That(HeroCarLodPolicy.IsWithinBudget(1, 195, 332), Is.False);
-            Assert.That(HeroCarLodPolicy.IsWithinBudget(2, 104, 221), Is.False);
+            Assert.That(HeroCarLodPolicy.IsWithinBudget(0, 274, 476), Is.False,
+                "The historically rejected 274v/476t Hero must remain below the production-art floor.");
+            Assert.That(HeroCarLodPolicy.IsWithinBudget(1, 194, 332), Is.False);
+            Assert.That(HeroCarLodPolicy.IsWithinBudget(2, 104, 180), Is.False);
         }
 
         [Test]
-        public void TriangleCountsDecreaseAtEveryLod()
+        public void UnderDetailedOrOverBudgetGeometryIsRejected()
         {
+            Assert.That(HeroCarLodPolicy.IsWithinBudget(0, HeroCarLodPolicy.MinimumVertices[0] - 1, HeroCarLodPolicy.MinimumTriangles[0]), Is.False);
+            Assert.That(HeroCarLodPolicy.IsWithinBudget(0, HeroCarLodPolicy.MinimumVertices[0], HeroCarLodPolicy.MinimumTriangles[0] - 1), Is.False);
+            Assert.That(HeroCarLodPolicy.IsWithinBudget(0, HeroCarLodPolicy.VertexBudgets[0] + 1, HeroCarLodPolicy.MinimumTriangles[0]), Is.False);
+            Assert.That(HeroCarLodPolicy.IsWithinBudget(0, HeroCarLodPolicy.MinimumVertices[0], HeroCarLodPolicy.TriangleBudgets[0] + 1), Is.False);
+        }
+
+        [Test]
+        public void ProductionTriangleFloorsDecreaseAtEveryLod()
+        {
+            Assert.That(HeroCarLodPolicy.MinimumTriangles[0], Is.GreaterThan(HeroCarLodPolicy.MinimumTriangles[1]));
+            Assert.That(HeroCarLodPolicy.MinimumTriangles[1], Is.GreaterThan(HeroCarLodPolicy.MinimumTriangles[2]));
             Assert.That(HeroCarLodPolicy.ExpectedTriangles[0], Is.GreaterThan(HeroCarLodPolicy.ExpectedTriangles[1]));
             Assert.That(HeroCarLodPolicy.ExpectedTriangles[1], Is.GreaterThan(HeroCarLodPolicy.ExpectedTriangles[2]));
         }
