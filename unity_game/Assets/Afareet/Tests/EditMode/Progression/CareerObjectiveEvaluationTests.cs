0960087a31abfcf8dea3e6a3f90cf530748fb5a8
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-17 00:28:26 +0300

    test(career): cover time drift and win evaluation

--- unity_game/Assets/Afareet/Tests/EditMode/Progression/CareerObjectiveEvaluationTests.cs
+++ unity_game/Assets/Afareet/Tests/EditMode/Progression/CareerObjectiveEvaluationTests.cs
@@ -7,79 +7,77 @@ namespace Afareet.Progression.Tests
     public sealed class CareerObjectiveEvaluationTests
     {
         [Test]
-        public void FinishedEvent_CompletesFinishObjective()
+        public void FinishedCircuit_CompletesFinishObjective()
         {
-            var definition = FindDefinition("c01_r01");
-
-            var result = CareerObjectiveEvaluationPolicy.Evaluate(
-                definition,
-                new CareerEventOutcome(finished: true, restartCount: 0));
-
+            var result = Evaluate("c01_r01", new CareerEventOutcome(true, 0));
             Assert.That(result.Entries.Count, Is.EqualTo(1));
-            Assert.That(result.Entries[0].ObjectiveId, Is.EqualTo("finish_c01_r01"));
-            Assert.That(result.Entries[0].Value, Is.EqualTo(1d));
-            Assert.That(result.Entries[0].IsComplete, Is.True);
-            Assert.That(result.AllCompleted, Is.True);
-        }
-
-        [Test]
-        public void CleanFinish_WithZeroRestarts_CompletesBothObjectives()
-        {
-            var definition = FindDefinition("c01_r02");
-
-            var result = CareerObjectiveEvaluationPolicy.Evaluate(
-                definition,
-                new CareerEventOutcome(finished: true, restartCount: 0));
-
-            Assert.That(result.Entries.Count, Is.EqualTo(2));
-            Assert.That(result.CompletedCount, Is.EqualTo(2));
-            Assert.That(result.AllCompleted, Is.True);
-            Assert.That(result.Entries[0].ObjectiveId, Is.EqualTo("finish_c01_r02"));
-            Assert.That(result.Entries[1].ObjectiveId, Is.EqualTo("clean_c01_r02"));
-        }
-
-        [Test]
-        public void RestartedFinish_BlocksCleanObjectiveOnly()
-        {
-            var definition = FindDefinition("c01_r03");
-
-            var result = CareerObjectiveEvaluationPolicy.Evaluate(
-                definition,
-                new CareerEventOutcome(finished: true, restartCount: 1));
-
             Assert.That(result.CompletedCount, Is.EqualTo(1));
-            Assert.That(result.AllCompleted, Is.False);
+            Assert.That(result.AllCompleted, Is.True);
+        }
+
+        [Test]
+        public void TimeTrial_RequiresConfiguredTargetTime()
+        {
+            var pass = Evaluate("c01_r02", new CareerEventOutcome(true, 0, finishTimeSeconds: 91.5d));
+            Assert.That(pass.CompletedCount, Is.EqualTo(3));
+            Assert.That(pass.AllCompleted, Is.True);
+            Assert.That(pass.Entries[2].ObjectiveId, Is.EqualTo("time_c01_r02"));
+
+            var miss = Evaluate("c01_r02", new CareerEventOutcome(true, 0, finishTimeSeconds: 92.1d));
+            Assert.That(miss.CompletedCount, Is.EqualTo(2));
+            Assert.That(miss.Entries[2].IsComplete, Is.False);
+        }
+
+        [Test]
+        public void Elimination_RequiresFirstPlaceAndCleanTracksRestartsSeparately()
+        {
+            var result = Evaluate("c01_r03", new CareerEventOutcome(true, 1, finalPosition: 1));
+            Assert.That(result.CompletedCount, Is.EqualTo(2));
             Assert.That(result.Entries[0].IsComplete, Is.True);
             Assert.That(result.Entries[1].IsComplete, Is.False);
-            Assert.That(result.Entries[1].Value, Is.EqualTo(0d));
+            Assert.That(result.Entries[2].ObjectiveId, Is.EqualTo("win_c01_r03"));
+            Assert.That(result.Entries[2].IsComplete, Is.True);
         }
 
         [Test]
-        public void UnfinishedEvent_CompletesNeitherObjective()
+        public void DriftChallenge_RequiresTargetDriftScore()
         {
-            var definition = FindDefinition("c01_r04");
+            var pass = Evaluate("c01_r04", new CareerEventOutcome(true, 0, driftScore: 12000));
+            Assert.That(pass.CompletedCount, Is.EqualTo(3));
+            Assert.That(pass.Entries[2].ObjectiveId, Is.EqualTo("drift_c01_r04"));
+            Assert.That(pass.Entries[2].IsComplete, Is.True);
 
-            var result = CareerObjectiveEvaluationPolicy.Evaluate(
-                definition,
-                new CareerEventOutcome(finished: false, restartCount: 0));
+            var miss = Evaluate("c01_r04", new CareerEventOutcome(true, 0, driftScore: 11999));
+            Assert.That(miss.CompletedCount, Is.EqualTo(2));
+            Assert.That(miss.AllCompleted, Is.False);
+        }
 
+        [Test]
+        public void Boss_RequiresFirstPlace()
+        {
+            var win = Evaluate("c01_boss", new CareerEventOutcome(true, 0, finalPosition: 1));
+            Assert.That(win.AllCompleted, Is.True);
+            Assert.That(win.Entries[2].ObjectiveId, Is.EqualTo("win_c01_boss"));
+
+            var loss = Evaluate("c01_boss", new CareerEventOutcome(true, 0, finalPosition: 2));
+            Assert.That(loss.CompletedCount, Is.EqualTo(2));
+            Assert.That(loss.AllCompleted, Is.False);
+        }
+
+        [Test]
+        public void MissingModeMetrics_FailsObjectiveClosedWithoutThrowing()
+        {
+            Assert.That(Evaluate("c01_r02", new CareerEventOutcome(true, 0)).AllCompleted, Is.False);
+            Assert.That(Evaluate("c01_r03", new CareerEventOutcome(true, 0)).AllCompleted, Is.False);
+            Assert.That(Evaluate("c01_r04", new CareerEventOutcome(true, 0)).AllCompleted, Is.False);
+        }
+
+        [Test]
+        public void UnfinishedEvent_CompletesNoObjectives()
+        {
+            var result = Evaluate("c01_r04", new CareerEventOutcome(false, 0, driftScore: 50000));
             Assert.That(result.CompletedCount, Is.Zero);
             Assert.That(result.AllCompleted, Is.False);
-            Assert.That(result.Entries[0].Value, Is.EqualTo(0d));
-            Assert.That(result.Entries[1].Value, Is.EqualTo(0d));
-        }
-
-        [Test]
-        public void Evaluation_PreservesDefinitionOrder()
-        {
-            var definition = FindDefinition("c01_boss");
-
-            var result = CareerObjectiveEvaluationPolicy.Evaluate(
-                definition,
-                new CareerEventOutcome(finished: true, restartCount: 0));
-
-            Assert.That(result.Entries[0].ObjectiveId, Is.EqualTo(definition.Objectives[0].Id));
-            Assert.That(result.Entries[1].ObjectiveId, Is.EqualTo(definition.Objectives[1].Id));
         }
 
         [Test]
@@ -90,34 +88,21 @@ namespace Afareet.Progression.Tests
                 source.Node,
                 new[] { new CareerObjective("future_c01_r02", "Future rule", 1d) },
                 source.Rewards);
-
             Assert.Throws<InvalidOperationException>(() =>
-                CareerObjectiveEvaluationPolicy.Evaluate(
-                    invalid,
-                    new CareerEventOutcome(finished: true, restartCount: 0)));
+                CareerObjectiveEvaluationPolicy.Evaluate(invalid, new CareerEventOutcome(true, 0)));
         }
 
         [Test]
-        public void NonBinaryTarget_FailsClosed()
+        public void InvalidOutcomeMetrics_FailClosed()
         {
-            var source = FindDefinition("c01_r02");
-            var invalid = new CareerNodeDefinition(
-                source.Node,
-                new[] { new CareerObjective("finish_c01_r02", "Finish twice", 2d) },
-                source.Rewards);
-
-            Assert.Throws<InvalidOperationException>(() =>
-                CareerObjectiveEvaluationPolicy.Evaluate(
-                    invalid,
-                    new CareerEventOutcome(finished: true, restartCount: 0)));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CareerEventOutcome(false, -1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CareerEventOutcome(true, 0, finishTimeSeconds: -1d));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CareerEventOutcome(true, 0, finalPosition: 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CareerEventOutcome(true, 0, driftScore: -1));
         }
 
-        [Test]
-        public void NegativeRestartCount_FailsClosed()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
-                new CareerEventOutcome(finished: false, restartCount: -1));
-        }
+        private static CareerObjectiveEvaluation Evaluate(string nodeId, CareerEventOutcome outcome) =>
+            CareerObjectiveEvaluationPolicy.Evaluate(FindDefinition(nodeId), outcome);
 
         private static CareerNodeDefinition FindDefinition(string nodeId)
         {
@@ -125,7 +110,6 @@ namespace Afareet.Progression.Tests
             for (var index = 0; index < definitions.Count; index++)
                 if (StringComparer.Ordinal.Equals(definitions[index].Node.Id, nodeId))
                     return definitions[index];
-
             throw new KeyNotFoundException(nodeId);
         }
     }
